e2bc3b7c9275ec442bd32bd6ff775b9246f1fb56
purplebeam <konvpapageorgiou@gmail.com>
2023-06-01 19:33:41 +0300

    changes v1

--- purplebeamcodes-blog/dom/header.php
+++ purplebeamcodes-blog/dom/header.php
@@ -7,7 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title> <?php echo $title;?> </title>
-
+    
+                <!-- Local Style CSS File -->
+    <link rel="stylesheet" href="css/styles.css">
                 <!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
                 <!-- Bootstrap JS CDN -->
--- purplebeamcodes-blog/log-in.php
+++ purplebeamcodes-blog/log-in.php
@@ -2,3 +2,41 @@
 require_once("dom/footer.php");
 require_once("dom/header.php");
 ?>
+
+<?php
+
+    class User {
+        public $name;
+        public $surname;
+        public $email;
+        private $id;
+        private $password;
+        private $role;
+        private $isLoggedIn;
+        private $isAdmin;
+        private $isBlocked;
+        private $isDeleted;
+
+        public function __construct($name,$surname,$email,$id,$password,$role,$isLoggedIn,$isAdmin,$isBlocked,$isDeleted){
+            $this->name = $name;
+            $this->surname = $surname;
+            $this->email = $email;
+            $this->id = $id;
+            $this->password = $password;
+            $this->role = $role;
+            $this->isLoggedIn = $isLoggedIn;
+            $this->isAdmin = $isAdmin;
+            $this->isBlocked = $isBlocked;
+            $this->isDeleted = $isDeleted;
+        }
+
+        public function pbinfo() 
+        {
+            echo "<br>Name: ".$this->name."<br> Surname: ".$this->surname."<br> Email: ".$this->email."<br> ID: ".$this->id."<br> Password: ".$this->password."<br> Role: ".$this->role;
+        }
+        
+    }
+
+    $userdata = new User("Konstantinos","Papageorgiou","konvpapageorgiou@gmail.com",001,"gitpass32","admin",true,true,false,false);
+    $userdata->pbinfo();
+?>
