92f26214c404f1b734b0e33a31fdaff04f395db6
purplebeam <konvpapageorgiou@gmail.com>
2023-06-03 00:46:06 +0300

    updated version 1.1

--- purplebeamcodes-blog/dom/header.php
+++ purplebeamcodes-blog/dom/header.php
@@ -71,11 +71,11 @@
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="#">Log in</a>
+                <a class="nav-link" href="log-in.php">Log in</a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="#">Sign Up</a>
+                <a class="nav-link" href="sign-up.php">Sign Up</a>
             </li>
       </ul>
 
