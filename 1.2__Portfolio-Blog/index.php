be39e8e0bb4574efcf6a2f09c87893c09276954b
purplebeam <konvpapageorgiou@gmail.com>
2023-06-05 14:46:02 +0300

    reshaping-repositories
    creating folders to work more easily on my projects and my exercises

---
+++ 1.2__Portfolio-Blog/index.php
@@ -0,0 +1,12 @@
+<?php 
+require_once("footer.php");
+require_once("header.php");
+?>
+<div class="container">
+    <div class="row" >
+<body style="background-color: #FBE7C6;">
+<h2 style = "margin: 10px 10px 20px 20px;" ><u>You are logged in as: </u><?php // Username here?><h2><br><br>
+    </div>
+</div>
+
+</body>
